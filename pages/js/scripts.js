5123caae07ea08cc7d4f513dbe3f7abc419b4494
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-07-18 14:22:36 +0200

    first commit

--- pages/js/scripts.js
+++ pages/js/scripts.js
@@ -21,23 +21,11 @@ window.onload = () => {
 
 				document.querySelectorAll('.swiper-navigation-button').forEach(button => button.classList.add('hide'));
 				document.querySelector('.swiper-pagination').classList.add('hide');
-				console.log(window.offsetHeight);
 				container.style.minHeight = window.innerHeight - 20 + 'px';
-
-				// container.addEventListener('mouseover', () => {
-				// 	document.querySelectorAll('.swiper-navigation-button').forEach(button => button.classList.remove('hide'));
-				// 	document.querySelector('.swiper-pagination').classList.remove('hide');
-				// });
-				//
-				// container.addEventListener('mouseout', () => {
-				// 	document.querySelectorAll('.swiper-navigation-button').forEach(button => button.classList.add('hide'));
-				// 	document.querySelector('.swiper-pagination').classList.add('hide');
-				// });
 			},
 			slideNextTransitionStart: () => {
 				if('first_transition_start' in window) {
 					let container = document.querySelector('.swiper-container');
-					console.log('slideNextTransitionStart');
 					if(container.hasAttribute('next_page')) {
 						window.next_page = container.getAttribute('next_page');
 					}
@@ -46,20 +34,17 @@ window.onload = () => {
 			},
 			slideNextTransitionEnd: () => {
 				if('first_transition_end' in window) {
-					console.log('slideNextTransitionEnd');
 					if('next_page' in window) window.location.href = window.next_page === '#' ? '' : window.next_page;
 				}
 				window.first_transition_end = true;
 			},
 			slidePrevTransitionStart: () => {
 				let container = document.querySelector('.swiper-container');
-				console.log('slidePrevTransitionStart');
 				if(container.hasAttribute('prev_page')) {
 					window.prev_page = container.getAttribute('prev_page');
 				}
 			},
 			slidePrevTransitionEnd: () => {
-				console.log('slidePrevTransitionEnd');
 				if('prev_page' in window) window.location.href = window.prev_page === '#' ? '' : window.prev_page;
 			}
 		}
